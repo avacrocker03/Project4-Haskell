0db87cfafa3d666207ea4520309bd0c28755c560
avacrocker03 <118841961+avacrocker03@users.noreply.github.com>
2023-12-01 18:02:41 -0500

    input and data type

--- Main.hs
+++ Main.hs
@@ -1,2 +1,27 @@
+-- q's for stanny: 
+-- ascii value clarification 
+-- compile: ghc -Wall --make Main.hs
+-- run: Main < input.txt
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = interact (readInput)
+
+-- reading input
+readInput :: String -> String
+readInput input = unlines (map readInputLines (lines input))
+
+-- reading in the input line by line
+readInputLines :: String -> String
+readInputLines line = unlines (map (\word -> "= " ++ word)  (splitLine line))
+
+-- splitting input at spaces and returning list
+splitLine :: String -> [String]
+splitLine sLine = words sLine
+
+-- creating data type for regular expression tree
+data RegexTree = Empty | Epsilon | Character Char | Alternation RegexTree RegexTree 
+            | OneOrMore RegexTree | KleeneStar RegexTree | Optional RegexTree 
+            | Concatentation RegexTree RegexTree
+            deriving (Show)
+
+-- fillTree :: String -> RegexTree
