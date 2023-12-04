e87384a086be2f3133cc21e82a8f9f1621ab39bf
Maxwell <caiazzamaxwell@gmail.com>
2023-12-04 12:54:37 -0500

    bingus boingus

--- Main.hs
+++ Main.hs
@@ -15,14 +15,36 @@ readInputLines line = unlines (map (\word -> "= " ++ word)  (splitLine line))
 -- splitting input at spaces and returning list
 splitLine :: String -> [String]
 splitLine sLine = words sLine
+let tree = convertToTree sLine !! 0
 
 -- creating data type for regular expression tree
-data RegexTree = Empty | Epsilon | Character Char | Alternation RegexTree RegexTree 
-            | OneOrMore RegexTree | KleeneStar RegexTree | Optional RegexTree 
+data RegexTree = Empty | Epsilon | Leaf Char | Alternation RegexTree RegexTree 
+            | Plus RegexTree | KleeneStar RegexTree | Optional RegexTree 
             | Concatentation RegexTree RegexTree
             deriving (Show)
 
 isOperator :: Char -> Bool
-isOperator op = op `elem` ['|', '+', '*', '?', '@', '\x2205', '\x03B5']
+isOperator op = op `elem` ['|', '+', '*', '?', '@']
 
 -- fillTree :: String -> RegexTree
+
+convertToTree :: String -> RegexTree
+convertToTree expr = buildTree [] expr
+  where
+    buildTree :: [RegexTree] -> String -> RegexTree
+    buildTree stack [] = head stack
+    buildTree stack (x:xs)
+        | isOperator x =
+            let newStack = case x of
+                        '|' -> let (r2:r1:rest) = stack
+                            in (Alternation r1 r2):rest
+                        '+' -> let (r2:r1:rest) = stack
+                            in (Plus r1 r2):rest
+                        '*' -> let (r:rest) = stack
+                            in (Star r):rest
+                        '?' -> let (r:rest) = stack
+                            in (Optional r):rest
+                        '@' -> let (r2:r1:rest) = stack
+                            in (Concatenation r1 r2):rest
+          in buildTree newStack xs
+        | otherwise = buildTree ((Leaf c):stack) xs
