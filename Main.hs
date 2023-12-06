7868fe82518c7f101b34626aa571a7483ba7141f
avacrocker03 <118841961+avacrocker03@users.noreply.github.com>
2023-12-06 10:39:54 -0500

    outlining stuff

--- Main.hs
+++ Main.hs
@@ -1,3 +1,11 @@
+{-
+ - Author: Ava Crocker, acrocker2021@my.fit.edu
+ - Author: Max Caiazza, mcaiazza2021@my.fit.edu
+ - Course: CSE 4250, Fall 2023
+ - Project: Proj4, Tautology Checker
+ - Language implementation: Glorious Glasgow Haskell Compilation System, version 8.4.3
+ -}
+
 -- compile: ghc -Wall --make Main.hs
 -- run: Main < input.txt
 
@@ -5,6 +13,9 @@
 -- Nail down encoding problems
     -- UTF-8
     -- 
+
+import Data.List (foldl)
+
 main :: IO ()
 main = interact readInput
 
@@ -20,18 +31,40 @@ readInputLines line =
     in unlines (show tree : map ("= " ++) wordsList)
 
 -- creating data type for regular expression tree
-data RegexTree = Empty | Epsilon | Leaf Char | Alternation RegexTree RegexTree
-            | Plus RegexTree | KleeneStar RegexTree | Optional RegexTree
-            | Concatenation RegexTree RegexTree
+data Regex = Empty | Epsilon | Leaf Char | Alternation Regex Regex
+            | Plus Regex | KleeneStar Regex | Optional Regex
+            | Concatenation Regex Regex
             deriving (Show)
 
 isOperator  :: Char -> Bool
 isOperator op = op `elem` ['|', '+', '*', '?', '@']
 
-regexToTree :: String -> RegexTree
+derive :: Regex -> Char -> Regex
+derive Empty _ = Empty
+derive Epsilon _ = Empty
+derive (Leaf x') x
+    | x == x' = Epsilon
+    | otherwise = Empty
+derive (Alternation r1 r2) x =
+    --
+derive (Plus r) x = 
+    -- 
+derive (KleeneStar r) x =
+    --
+derive (Optional r) x = 
+    --
+derive (Concatentation r1 r2) x =
+    --
+nullable :: Regex -> Bool
+-- if regex = epsilon
+-- used to compare input derivation to output
+match :: Regex -> String -> Bool
+match x str = nullable (foldl derive x str)
+
+regexToTree :: String -> Regex
 regexToTree = popTree []
   where
-    popTree :: [RegexTree] -> String -> RegexTree
+    popTree :: [Regex] -> String -> Regex
     popTree stack [] = head stack
     popTree stack (x:xs)
         | isOperator x =
